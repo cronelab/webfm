1dc44e3e227381435db99d177708af399b8b2b4f
Chris <17508521+TheBrainChain@users.noreply.github.com>
2019-08-30 00:19:15 -0400

    html and css upgrades

--- src/record/main.js
+++ src/record/main.js
@@ -4,63 +4,38 @@ import "@fortawesome/fontawesome-free/js/all";
 import fmui from '../shared/fmui'
 import fmdata from '../shared/fmdata';
 
-
-let dataset;
 let uiManager;
-var subjectName;
-var recordName;
-
 window.onload = async () => {
-    subjectName = localStorage.getItem('subject');
-    recordName = localStorage.getItem('record');
-
+    let subjectName = localStorage.getItem('subject');
+    let recordName = localStorage.getItem('record');
 
     let request = await fetch(`/config`)
     let data = await request.json()
-    dataset = new fmdata();
+    let dataset = new fmdata();
     uiManager = new fmui();
 
     uiManager.config.ui = data;
     uiManager.setup();
-    uiManager.raster.oncursormove = newTime => {
-        document.getElementsByClassName('fm-time-selected')[0].innerHTML = (newTime > 0 ? '+' : '') + newTime.toFixed(3) + ' s';
-        uiManager.brain.update(dataset.dataForTime(newTime));
-    };
 
     fetch(`/api/data/${subjectName}/${recordName}`).then(response => response.json()).then(data => {
-        dataset.get(data).then((x) => {
-
-
+        dataset.get(data).then(x => {
             document.getElementsByClassName('fm-subject-name')[0].innerHTML = subjectName;
             document.getElementsByClassName('fm-subject-name')[1].innerHTML = subjectName;
             document.getElementsByClassName('fm-back')[0].setAttribute('href', `/#${subjectName}`);
-
             document.getElementsByClassName('fm-task-name')[0].innerHTML = dataset.metadata.setting.task;
             document.getElementById('fm-option-save-name').value = dataset.metadata.setting.task;
-
             uiManager.updateChannelNames(dataset.metadata.montage);
             uiManager.raster.update(dataset.displayData);
             uiManager.brain.setup(JSON.parse(localStorage.getItem('brain')).brain, JSON.parse(localStorage.getItem('geometry')).geometry);
             uiManager.brain.update(dataset.dataForTime(uiManager.raster.cursorTime));
-            let timeBounds = dataset.getTimeBounds();
-
-
-
-            if (!uiManager.raster.timeScale) {
-                return;
-            }
-            uiManager.raster.timeScale.range([timeBounds.start, timeBounds.end]);
-
-
-
+            if (!uiManager.raster.timeScale) return;
+            uiManager.raster.timeScale.range([dataset.getTimeBounds().start, dataset.getTimeBounds().end]);
         })
     })
-
+    uiManager.raster.oncursormove = newTime => {
+        document.getElementsByClassName('fm-time-selected')[0].innerHTML = `${(newTime > 0 ? '+' : '')}${newTime.toFixed(3)} s`;
+        uiManager.brain.update(dataset.dataForTime(newTime));
+    };
 }
-
-
-
-
-
 
 window.onresize = () => uiManager.didResize();
