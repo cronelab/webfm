5d26f5664fecc6328d404a89f742b7038f9dca55
TheBrainChain <c.coogan2201@gmail.com>
2020-01-26 12:08:33 -0500

    removed old files and separated 3D files

--- webpack.config.js
+++ webpack.config.js
@@ -11,10 +11,11 @@ const devMode = process.env.NODE_ENV !== "production";
 const module = {
 	mode: devMode ? "development" : "production",
 	devtool: devMode ? "inline-source-map" : "source-map",
-	entry: './src/index.tsx',
-	devServer: {
-		historyApiFallback: true,
+	entry: {
+		main: './src/index.tsx',
+		reconstruction3D: "./src/reconstruction3D/main.js"
 	},
+
 	// {
 	// index: "./src/index/main.ts",
 	// record: "./src/record/main.ts",
@@ -24,8 +25,9 @@ const module = {
 	// streamSaver: "./streamSaver/index.js",
 	// cceps: "./src/CCEPS/index.js",
 	// threeD: "./src/3DViewer/main.js",
-	// loader_nifti: "./src/loader_nifti/main.js"
-
+	// devServer: {
+	// 	historyApiFallback: true,
+	// },
 	// },
 	node: {
 		fs: 'empty'
@@ -110,69 +112,72 @@ const module = {
 			chunkFilename: "[id].css"
 		}),
 		new HtmlWebpackPlugin({
-			template: './src/index.html'
-		})
+			template: './src/index.html',
+			filename: "index.html",
+			hash: true,
+			chunks: ["main"]
+
+		}),
+		new HtmlWebpackPlugin({
+			hash: true,
+			template: "./src/reconstruction3D/index.html",
+			filename: "reconstruction3D.html",
+			chunks: ["reconstruction3D"],
+			title: "WebFM"
+		}),
+		// 	new HtmlWebpackPlugin({
+		// 		hash: true,
+		// 		template: "./src/index/index.html",
+		// 		filename: "index.html",
+		// 		chunks: ["index_original"],
+		// 		title: "WebFM"
+		// 	}),
+		// 	new HtmlWebpackPlugin({
+		// 		hash: true,
+		// 		template: "./src/record/index.html",
+		// 		filename: "record.html",
+		// 		chunks: ["record"],
+		// 		title: "WebFM"
+		// 	}),
+		// 	new HtmlWebpackPlugin({
+		// 		hash: true,
+		// 		template: "./src/map/index.html",
+		// 		filename: "map.html",
+		// 		chunks: ["map"],
+		// 		title: "WebFM"
+		// 	}),
+		// 	new HtmlWebpackPlugin({
+		// 		hash: true,
+		// 		template: "./src/CCEPS/index.html",
+		// 		filename: "cceps.html",
+		// 		chunks: ["cceps"],
+		// 		title: "WebFM"
+		// 	}),
+		// 	new HtmlWebpackPlugin({
+		// 		hash: true,
+		// 		template: "./src/3DViewer/index.html",
+		// 		filename: "threeD.html",
+		// 		chunks: ["threeD"],
+		// 		title: "WebFM"
+		// 	}),
+
+		// 	new HtmlWebpackPlugin({
+		// 		hash: true,
+		// 		template: "./src/index.html",
+		// 		filename: "index.html",
+		// 		chunks: ["index_r"],
+		// 		title: "WebFM"
+		// 	}),
+		// 	new HtmlWebpackPlugin({
+		// 		hash: true,
+		// 		filename: "record_r.html",
+		// 		chunks: ["record_r"],
+		// 		title: "WebFM"
+		// 	})
 	],
-	// 	new HtmlWebpackPlugin({
-	// 		hash: true,
-	// 		template: "./src/index/index.html",
-	// 		filename: "index.html",
-	// 		chunks: ["index_original"],
-	// 		title: "WebFM"
-	// 	}),
-	// 	new HtmlWebpackPlugin({
-	// 		hash: true,
-	// 		template: "./src/record/index.html",
-	// 		filename: "record.html",
-	// 		chunks: ["record"],
-	// 		title: "WebFM"
-	// 	}),
-	// 	new HtmlWebpackPlugin({
-	// 		hash: true,
-	// 		template: "./src/map/index.html",
-	// 		filename: "map.html",
-	// 		chunks: ["map"],
-	// 		title: "WebFM"
-	// 	}),
-	// 	new HtmlWebpackPlugin({
-	// 		hash: true,
-	// 		template: "./src/CCEPS/index.html",
-	// 		filename: "cceps.html",
-	// 		chunks: ["cceps"],
-	// 		title: "WebFM"
-	// 	}),
-	// 	new HtmlWebpackPlugin({
-	// 		hash: true,
-	// 		template: "./src/3DViewer/index.html",
-	// 		filename: "threeD.html",
-	// 		chunks: ["threeD"],
-	// 		title: "WebFM"
-	// 	}),
-	// 	new HtmlWebpackPlugin({
-	// 		hash: true,
-	// 		template: "./src/loader_nifti/index.html",
-	// 		filename: "loader_nifti.html",
-	// 		chunks: ["loader_nifti"],
-	// 		title: "WebFM"
-	// 	}),
-	// 	new HtmlWebpackPlugin({
-	// 		hash: true,
-	// 		template: "./src/index.html",
-	// 		filename: "index.html",
-	// 		chunks: ["index_r"],
-	// 		title: "WebFM"
-	// 	}),
-	// 	new HtmlWebpackPlugin({
-	// 		hash: true,
-	// 		filename: "record_r.html",
-	// 		chunks: ["record_r"],
-	// 		title: "WebFM"
-	// 	})
-	// ],
 	output: {
 		path: path.resolve(__dirname, "dist"),
-		filename: "index_bundle.js",
-		// filename: "[name].[hash].js",
+		filename: "[name].[hash].js",
 		globalObject: `typeof self !== 'undefined' ? self : this`,
 		publicPath: '/'
 
