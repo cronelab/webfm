f73366fb9c86d905837fd6648d5bf4d5de33ba30
TheBrainChain <c.coogan2201@gmail.com>
2020-11-05 14:48:35 -0500

    mobile friendly viewport

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -1,5 +1,7 @@
 declare module "bci2k";
 declare module '*.jpg';
+declare module '*.png';
+declare module '*.gif';
 declare module "worker-loader!*" {
   class WebpackWorker extends Worker {
     constructor();
